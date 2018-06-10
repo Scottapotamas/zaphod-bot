ea388b42256f6ffdc4023572ff2b542ba01f695c
Scott Rapson <scottapotamas@gmail.com>
2018-06-10 23:20:58 +0930

    First pass implementation of electricui
    Uses communication task to setup callbacks and UART peripheral.
    Validated working with LED toggle.

--- software/delta-control/Src/app_state_machines/app_task_communication.c
+++ software/delta-control/Src/app_state_machines/app_task_communication.c
@@ -11,19 +11,56 @@
 
 #include "app_task_communication.h"
 
+#include "electricui.h"
+#include <eui_serial_transport.h>
+
+#include "status.h"
+#include "hal_uart.h"
 
 /* ----- Private Function Definitions --------------------------------------- */
 
 PRIVATE void AppTaskCommunicationConstructor( AppTaskCommunication *me );
 
-PRIVATE void AppTaskCommunication_initial( AppTaskCommunication *me,
-                                        const StateEvent *e );
+PRIVATE void AppTaskCommunication_initial( AppTaskCommunication *me, const StateEvent *e );
 
-PRIVATE STATE AppTaskCommunication_main( AppTaskCommunication *me,
-                                      const StateEvent *e );
+PRIVATE void AppTaskCommunication_tx_put_internal( uint8_t c );
 
-PRIVATE STATE AppTaskCommunication_next_state( AppTaskCommunication *me,
-                                      const StateEvent *e );
+PRIVATE void AppTaskCommunication_tx_put_external( uint8_t c );
+
+PRIVATE void AppTaskCommunication_tx_put_module( uint8_t c );
+
+PRIVATE void AppTaskCommunication_rx_callback( HalUartPort_t port, uint8_t c );
+
+
+
+PRIVATE STATE AppTaskCommunication_main( AppTaskCommunication *me, const StateEvent *e );
+
+PRIVATE STATE AppTaskCommunication_electric_ui( AppTaskCommunication *me, const StateEvent *e );
+
+eui_interface module_comms;
+
+PRIVATE void toggleLed();
+
+uint8_t   example_uint8   = 21;
+uint16_t  example_uint16  = 321;
+uint32_t  example_uint32  = 654321;
+float     example_float   = 3.14159;
+
+euiMessage_t dev_msg_store[] = {
+
+    {.msgID = "tgl", .type = TYPE_CALLBACK, .size = sizeof(toggleLed),      .payload = &toggleLed       },
+
+    //type examples
+    {.msgID = "ui8", .type = TYPE_UINT8,  .size = sizeof(example_uint8),  .payload = &example_uint8       },
+    {.msgID = "i16", .type = TYPE_UINT16, .size = sizeof(example_uint16), .payload = &example_uint16      },
+    {.msgID = "i32", .type = TYPE_UINT32, .size = sizeof(example_uint32), .payload = &example_uint32      },
+    {.msgID = "fPI", .type = TYPE_FLOAT,  .size = sizeof(example_float),  .payload = &example_float       },
+};
+
+void toggleLed()
+{
+  status_yellow_toggle();
+}
 
 /* ----- Public Functions --------------------------------------------------- */
 
@@ -78,7 +115,7 @@ PRIVATE STATE AppTaskCommunication_main( AppTaskCommunication *me,
         }
 
         case STATE_INIT_SIGNAL:
-            STATE_INIT( &AppTaskCommunication_next_state );
+            STATE_INIT( &AppTaskCommunication_electric_ui );
             return 0;
 
     }
@@ -87,28 +124,71 @@ PRIVATE STATE AppTaskCommunication_main( AppTaskCommunication *me,
 
 /* -------------------------------------------------------------------------- */
 
-PRIVATE STATE AppTaskCommunication_next_state( AppTaskCommunication *me,
+PRIVATE STATE AppTaskCommunication_electric_ui( AppTaskCommunication *me,
                                          	 const StateEvent *e )
 {
     switch( e->signal )
     {
         case STATE_ENTRY_SIGNAL:
 
+        	//uart setup
+            hal_uart_init( HAL_UART_PORT_MODULE );
+            hal_uart_set_baudrate( HAL_UART_PORT_MODULE, 115200 );
+        	hal_uart_set_rx_handler(HAL_UART_PORT_MODULE, AppTaskCommunication_rx_callback );
+
+			//eUI setup
+			module_comms.output_char_fnPtr = &AppTaskCommunication_tx_put_module;
+			setup_dev_msg(dev_msg_store, ARR_ELEM(dev_msg_store));
+			setup_identifier();
+
         	return 0;
-
-        case STATE_TIMEOUT1_SIGNAL:
-
-			return 0;
-
-        case STATE_TIMEOUT2_SIGNAL:
-
-			return 0;
 
 		case STATE_EXIT_SIGNAL:
 
 			return 0;
     }
     return (STATE)AppTaskCommunication_main;
+}
+
+PRIVATE void
+AppTaskCommunication_tx_put_external( uint8_t c )
+{
+    hal_uart_put( HAL_UART_PORT_EXTERNAL, c );
+}
+
+PRIVATE void
+AppTaskCommunication_tx_put_internal( uint8_t c )
+{
+    hal_uart_put( HAL_UART_PORT_INTERNAL, c );
+}
+
+PRIVATE void
+AppTaskCommunication_tx_put_module( uint8_t c )
+{
+    hal_uart_put( HAL_UART_PORT_MODULE, c );
+}
+
+PRIVATE void
+AppTaskCommunication_rx_callback( HalUartPort_t port, uint8_t c )
+{
+	switch ( port )
+	{
+		case HAL_UART_PORT_MODULE:
+				parse_packet(c, &module_comms);
+			break;
+
+		case HAL_UART_PORT_EXTERNAL:
+
+			break;
+
+		case HAL_UART_PORT_INTERNAL:
+
+			break;
+
+		default:
+			break;
+	}
+
 }
 
 /* ----- End ---------------------------------------------------------------- */
