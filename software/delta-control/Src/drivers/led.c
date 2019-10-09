70c4f6e7cbfa491e6de199b037cb6cae6e3b1ca7
Scott <scottapotamas@gmail.com>
2019-10-09 18:10:23 +1030

    First pass LED linearisation, white-point and power offset support
    Allows the LED colour balance and brightness to be adjusted by the UI.

--- software/delta-control/Src/drivers/led.c
+++ software/delta-control/Src/drivers/led.c
@@ -1,6 +1,7 @@
 /* ----- System Includes ---------------------------------------------------- */
 
 #include <string.h>
+#include <math.h>
 
 /* ----- Local Includes ----------------------------------------------------- */
 
@@ -13,6 +14,15 @@
 /* ----- Private Types ------------------------------------------------------ */
 
 /* ----- Private Variables -------------------------------------------------- */
+
+PRIVATE float
+led_luminance_correct( float input );
+
+PRIVATE void
+led_whitebalance_correct( float *red, float *green, float *blue );
+
+PRIVATE float
+led_power_limit( );
 
 
 /* ----- Public Functions --------------------------------------------------- */
@@ -36,30 +46,85 @@ led_enable( bool enable )
 
 /* -------------------------------------------------------------------------- */
 
+// Input 3 float [0-1] values for RGB color channels in linear space
+// Applies luma and whitebalance correction
 PUBLIC void
 led_set( float r, float g, float b )
 {
-    uint8_t r_duty = r*255;
-    uint8_t g_duty = g*255;
-    uint8_t b_duty = b*255;
+    float setpoint_r = led_luminance_correct( r );
+    float setpoint_g = led_luminance_correct( g );
+    float setpoint_b = led_luminance_correct( b );
 
-    //todo clamp led illumination power with override value
 
-    //todo apply linear/log conversion for led
+    led_whitebalance_correct( &setpoint_r, &setpoint_g, &setpoint_b );
+    setpoint_r *= led_power_limit();
+    setpoint_g *= led_power_limit();
+    setpoint_b *= led_power_limit();
 
-    uint32_t red    = r*0xFFFF;
-    uint32_t green  = g*0xFFFF;
-    uint32_t blue   = b*0xFFFF;
+    uint16_t led_r = CLAMP( setpoint_r, 0.0f, 1.0f )*0xFFFF;
+    uint16_t led_g = CLAMP( setpoint_g, 0.0f, 1.0f )*0xFFFF;
+    uint16_t led_b = CLAMP( setpoint_b, 0.0f, 1.0f )*0xFFFF;
 
     // Set the final output percentages for the led PWM channels
-    hal_pwm_set( _PWM_TIM_AUX_0, red);
-    hal_pwm_set( _PWM_TIM_AUX_2, green);
-    hal_pwm_set( _PWM_TIM_AUX_1, blue);
+    hal_pwm_set( _PWM_TIM_AUX_0, led_r);
+    hal_pwm_set( _PWM_TIM_AUX_2, led_g);
+    hal_pwm_set( _PWM_TIM_AUX_1, led_b);
 
-    config_set_led_values(red, green, blue);
+    config_set_led_values(led_r, led_g, led_b);
 }
 
-    config_set_led_values(r_duty, g_duty, b_duty);
+/* -------------------------------------------------------------------------- */
+
+PRIVATE float
+led_luminance_correct( float input )
+{
+    float lightness = input*100;
+
+    /* CIE 1931 Luminance Formula
+     * https://en.wikipedia.org/wiki/CIE_1931_color_space
+     *  Y = (L* / 902.3)           if L* ≤ 8
+     *  Y = ((L* + 16) / 116)^3    if L* > 8
+     */
+
+    float luminance = 0.0f;
+
+    if ( lightness <= 8 )
+    {
+        luminance = ( lightness / 903.296f );
+    }
+    else
+    {
+        luminance = pow( ( (lightness + 16)/116 ), 3);
+    }
+
+    return luminance;
+}
+
+/* -------------------------------------------------------------------------- */
+
+PRIVATE void
+led_whitebalance_correct( float *red, float *green, float *blue )
+{
+    int16_t wb_r, wb_g, wb_b = 0;
+    config_get_led_whitebalance( &wb_r, &wb_g, &wb_b );
+
+    // Apply offsets
+    *red   = *red *   ( (float)wb_r / 32767.0f );
+    *green = *green * ( (float)wb_g / 32767.0f );
+    *blue  = *blue *  ( (float)wb_b / 32767.0f );
+}
+
+/* -------------------------------------------------------------------------- */
+
+PRIVATE float
+led_power_limit()
+{
+    int16_t limit = 0;
+    config_get_led_bias( &limit );
+
+    // Apply offset based on that value
+
+    return (float)limit / 32767;
 }
 
 /* ----- End ---------------------------------------------------------------- */
