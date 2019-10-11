06d739e688cf0d16a92391a91e3ad68d06e0e6c8
Scott <scottapotamas@gmail.com>
2019-10-11 15:47:52 +1030

    Refactored manual control card to use atomic layout

--- software/delta-ui/src/application/pages/DevicePages/ControlPage/ManualJogPalette.tsx
+++ software/delta-ui/src/application/pages/DevicePages/ControlPage/ManualJogPalette.tsx
@@ -1,9 +1,136 @@
-import { Cell, Grid } from 'styled-css-grid'
-import { Icon, MultiSlider, NumericInput } from '@blueprintjs/core'
 import React, { useState } from 'react'
+
+import { Icon, MultiSlider, NumericInput } from '@blueprintjs/core'
+import { Composition } from 'atomic-layout'
 
 import { Button } from '@electricui/components-desktop-blueprint'
 import { CALL_CALLBACK } from '@electricui/core'
+
+const JogButtonAreas = `
+XP YP ZP
+XM YM ZM
+`
+
+interface JogPaletteProps {
+  distance: number
+  time: number
+}
+
+const JogPalette = (props: JogPaletteProps) => {
+  return (
+    <Composition areas={JogButtonAreas} gap={5} templateCols="auto auto 1fr">
+      {({ XP, YP, ZP, XM, YM, ZM }) => (
+        <React.Fragment>
+          <XP>
+            <Button
+              large
+              writer={{
+                inmv: {
+                  type: 0,
+                  reference: 1,
+                  id: 0,
+                  duration: props.time,
+                  points: [[props.distance, 0, 0]],
+                },
+              }}
+            >
+              <Icon icon="arrow-top-left" iconSize={50} />
+            </Button>
+          </XP>
+          <YP>
+            <Button
+              large
+              writer={{
+                inmv: {
+                  type: 0,
+                  reference: 1,
+                  id: 0,
+                  duration: props.time,
+                  points: [[0, props.distance, 0]],
+                },
+              }}
+            >
+              <Icon icon="arrow-top-right" iconSize={50} />
+            </Button>
+          </YP>
+          <XM>
+            <Button
+              large
+              writer={{
+                inmv: {
+                  type: 0,
+                  reference: 1,
+                  id: 0,
+                  duration: props.time,
+                  points: [[-props.distance, 0, 0]],
+                },
+              }}
+            >
+              <Icon icon="arrow-bottom-left" iconSize={50} />
+            </Button>
+          </XM>
+          <YM>
+            <Button
+              large
+              writer={{
+                inmv: {
+                  type: 0,
+                  reference: 1,
+                  id: 0,
+                  duration: props.time,
+                  points: [[0, -props.distance, 0]],
+                },
+              }}
+            >
+              <Icon icon="arrow-top-left" iconSize={50} />
+            </Button>
+          </YM>
+          <ZP>
+            <Button
+              large
+              fill
+              writer={{
+                inmv: {
+                  type: 0,
+                  reference: 1,
+                  id: 0,
+                  duration: props.time,
+                  points: [[0, 0, props.distance]],
+                },
+              }}
+            >
+              <Icon icon="arrow-up" iconSize={50} />
+            </Button>
+          </ZP>
+          <ZM>
+            {' '}
+            <Button
+              large
+              fill
+              writer={{
+                inmv: {
+                  type: 0,
+                  reference: 1,
+                  id: 0,
+                  duration: props.time,
+                  points: [[0, 0, -props.distance]],
+                },
+              }}
+            >
+              <Icon icon="arrow-down" iconSize={50} />
+            </Button>
+          </ZM>
+        </React.Fragment>
+      )}
+    </Composition>
+  )
+}
+
+const controllerAreas = `
+SpeedArea DistanceArea a
+SpeedArea DistanceArea KeypadArea
+SpeedArea DistanceArea b
+`
 
 const ManualJogPalette = () => {
   let [jog_speed, setJogSpeed] = useState(0)
@@ -12,183 +139,78 @@ const ManualJogPalette = () => {
   let jog_duration = 600 * (1 / (1 + jog_speed))
 
   return (
-    <div>
-      <Grid columns={'80px 80px 160px 100px'} justifyContent="space-around">
-        <Cell center>
-          <label>Speed</label>
-          <br />
-          <br />
-          <MultiSlider
-            min={-0.75}
-            max={1}
-            stepSize={0.25}
-            vertical
-            labelRenderer={val => `${Math.round((val + 1) * 100)}%`}
-            labelStepSize={0.5}
-            showTrackFill={jog_speed !== 0}
-          >
-            <MultiSlider.Handle
+    <Composition
+      areas={controllerAreas}
+      gap={30}
+      padding={15}
+      templateCols="1fr 1fr 2fr"
+    >
+      {({ SpeedArea, DistanceArea, KeypadArea }) => (
+        <React.Fragment>
+          <SpeedArea>
+            <b>Speed</b>
+            <br />
+            <br />
+            <MultiSlider
+              min={-0.75}
+              max={1}
+              stepSize={0.25}
+              vertical
+              labelRenderer={val => `${Math.round((val + 1) * 100)}%`}
+              labelStepSize={0.5}
+              showTrackFill={jog_speed !== 0}
+            >
+              <MultiSlider.Handle
+                value={jog_speed}
+                intentAfter={jog_speed < 0 ? 'warning' : undefined}
+                intentBefore={jog_speed > 0 ? 'warning' : undefined}
+                onChange={value => setJogSpeed(value)}
+              />
+              <MultiSlider.Handle value={0} interactionKind="none" />
+            </MultiSlider>
+            <br />
+            <NumericInput
               value={jog_speed}
-              intentAfter={jog_speed < 0 ? 'warning' : undefined}
-              intentBefore={jog_speed > 0 ? 'warning' : undefined}
-              onChange={value => setJogSpeed(value)}
+              onValueChange={value => setJogSpeed(value)}
+              fill
+              intent="warning"
+              min={-1}
+              max={1}
+              stepSize={0.25}
             />
-            <MultiSlider.Handle value={0} interactionKind="none" />
-          </MultiSlider>
-          <br />
-          <NumericInput
-            value={jog_speed}
-            onValueChange={value => setJogSpeed(value)}
-            fill
-            intent="warning"
-            min={-1}
-            max={1}
-            stepSize={0.25}
-          />
-        </Cell>
-        <Cell center>
-          <label>Distance</label>
-          <br />
-          <br />
-          <MultiSlider
-            onChange={values => setJogDistance(values[0])}
-            min={0}
-            max={10}
-            stepSize={1}
-            vertical
-            labelRenderer={val => `${val}mm`}
-            labelStepSize={5}
-          >
-            <MultiSlider.Handle value={jog_distance} intentBefore="success" />
-          </MultiSlider>
-          <br />
-          <NumericInput
-            value={jog_distance}
-            onValueChange={value => setJogDistance(value)}
-            fill
-            intent="success"
-            min={0}
-            max={10}
-            stepSize={1}
-          />
-        </Cell>
-        <Cell middle>
-          <Grid columns={'50px 50px 50px'} rows={'50px 50px 50px'}>
-            <Cell left={2}>
-              <Button
-                large
-                fill
-                writer={{
-                  inmv: {
-                    type: 0,
-                    reference: 1,
-                    id: 0,
-                    duration: jog_duration,
-                    num_points: 1,
-                    points: [[jog_distance, 0, 0]],
-                  },
-                }}
-              >
-                <Icon icon="arrow-up" iconSize={40} />
-              </Button>
-            </Cell>
-            <Cell left={1} top={2}>
-              <Button
-                large
-                fill
-                writer={{
-                  inmv: {
-                    type: 0,
-                    reference: 1,
-                    id: 0,
-                    duration: jog_duration,
-                    num_points: 1,
-                    points: [[0, jog_distance, 0]],
-                  },
-                }}
-              >
-                <Icon icon="arrow-left" iconSize={40} />
-              </Button>
-            </Cell>
-            <Cell left={3} top={2}>
-              <Button
-                large
-                fill
-                writer={{
-                  inmv: {
-                    type: 0,
-                    reference: 1,
-                    id: 0,
-                    duration: jog_duration,
-                    num_points: 1,
-                    points: [[0, -jog_distance, 0]],
-                  },
-                }}
-              >
-                <Icon icon="arrow-right" iconSize={40} />
-              </Button>
-            </Cell>
-            <Cell left={2} top={3}>
-              <Button
-                large
-                fill
-                writer={{
-                  inmv: {
-                    type: 0,
-                    reference: 1,
-                    id: 0,
-                    duration: jog_duration,
-                    num_points: 1,
-                    points: [[-jog_distance, 0, 0]],
-                  },
-                }}
-              >
-                <Icon icon="arrow-down" iconSize={40} />
-              </Button>
-            </Cell>
-          </Grid>
-        </Cell>
-        <Cell middle>
-          <Grid columns={'100px'} rows={'50px'} alignContent="space-evenly">
-            <Cell>
-              <Button
-                large
-                fill
-                writer={{
-                  inmv: {
-                    type: 0,
-                    reference: 1,
-                    id: 0,
-                    duration: jog_duration,
-                    points: [[0, 0, jog_distance]],
-                  },
-                }}
-              >
-                <Icon icon="arrow-up" iconSize={40} />
-              </Button>
-            </Cell>
-
-            <Cell>
-              <Button
-                large
-                fill
-                writer={{
-                  inmv: {
-                    type: 0,
-                    reference: 1,
-                    id: 0,
-                    duration: jog_duration,
-                    points: [[0, 0, -jog_distance]],
-                  },
-                }}
-              >
-                <Icon icon="arrow-down" iconSize={40} />
-              </Button>
-            </Cell>
-          </Grid>
-        </Cell>
-      </Grid>
-    </div>
+          </SpeedArea>
+          <DistanceArea>
+            <b>Distance</b>
+            <br />
+            <br />
+            <MultiSlider
+              onChange={values => setJogDistance(values[0])}
+              min={0}
+              max={10}
+              stepSize={1}
+              vertical
+              labelRenderer={val => `${val}mm`}
+              labelStepSize={5}
+            >
+              <MultiSlider.Handle value={jog_distance} intentBefore="success" />
+            </MultiSlider>
+            <br />
+            <NumericInput
+              value={jog_distance}
+              onValueChange={value => setJogDistance(value)}
+              fill
+              intent="success"
+              min={0}
+              max={10}
+              stepSize={1}
+            />
+          </DistanceArea>
+          <KeypadArea>
+            <JogPalette distance={jog_distance} time={jog_duration} />
+          </KeypadArea>
+        </React.Fragment>
+      )}
+    </Composition>
   )
 }
 
