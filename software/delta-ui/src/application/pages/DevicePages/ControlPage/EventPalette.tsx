35b248ab2f2a08b18098996277ca4a5db2ae0522
Scott <scottapotamas@gmail.com>
2019-11-08 22:32:28 +1030

    Event/SceneController UI fix

--- software/delta-ui/src/application/pages/DevicePages/ControlPage/EventPalette.tsx
+++ software/delta-ui/src/application/pages/DevicePages/ControlPage/EventPalette.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
-import { IntervalRequester } from '@electricui/components-core'
+import {
+  useDeviceMetadataKey,
+  IntervalRequester,
+} from '@electricui/components-core'
 
 import { SceneController } from './../SceneControl/SceneController'
 
 const EventPalette = () => {
+  const sceneFilePath = useDeviceMetadataKey('summary_file_path')
+
   return (
     <>
       <IntervalRequester interval={100} variables={['rgb']} />
       <h3>Load Event Sequence from File</h3>
-      <SceneController />
+      <SceneController key={sceneFilePath} />
     </>
   )
 }
