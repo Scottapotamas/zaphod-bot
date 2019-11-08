83ffb94b44e3e450c336f406755d6108c7a27ed4
Scott <scottapotamas@gmail.com>
2019-11-09 00:39:27 +1030

    Path interpolator uses dual move buffer to reduce delay between consecutive moves

--- software/delta-control/Src/drivers/path_interpolator.c
+++ software/delta-control/Src/drivers/path_interpolator.c
@@ -23,7 +23,8 @@
 typedef enum
 {
     PLANNER_OFF,
-    PLANNER_ON,
+    PLANNER_EXECUTE_A,
+    PLANNER_EXECUTE_B
 } PlanningState_t;
 
 typedef struct
@@ -32,9 +33,12 @@ typedef struct
 	PlanningState_t   currentState;
 	PlanningState_t   nextState;
 
-	Movement_t		current_move;		// copy of the current movement
+    Movement_t		move_a;		// copy of the current movement
+    Movement_t		move_b;		// copy of the current movement
+
 	bool			enable;				//if the planner is enabled
     uint32_t        movement_started;	// timestamp the start point
+    uint32_t        movement_est_complete;	// timestamp the predicted end point
     float        	progress_percent;	// calculated progress
 
     CartesianPoint_t	effector_position;	//position of the end effector (used for relative moves)
@@ -44,6 +48,9 @@ typedef struct
 /* ----- Private Variables -------------------------------------------------- */
 
 PRIVATE MotionPlanner_t planner;
+
+PRIVATE void execute_move( Movement_t *move, float percentage );
+PRIVATE void path_interpolator_calculate_percentage( uint16_t move_duration );
 
 /* ----- Public Functions --------------------------------------------------- */
 
@@ -59,37 +66,45 @@ PUBLIC void
 path_interpolator_set_next(Movement_t *movement_to_process )
 {
 	MotionPlanner_t *me = &planner;
+	Movement_t *movement_insert_slot = { 0 };   // allows us to put the new move into whichever slot is available
 
-	memcpy( &me->current_move, movement_to_process, sizeof(Movement_t) );
+	if( me->currentState == PLANNER_EXECUTE_B || me->currentState == PLANNER_OFF )
+    {
+	    movement_insert_slot = &me->move_a;
+    }
+	else if ( me->currentState == PLANNER_EXECUTE_A )
+    {
+        movement_insert_slot = &me->move_b;
+    }
+
+	memcpy( movement_insert_slot, movement_to_process, sizeof(Movement_t) );
 
 	//apply current position to a relative movement
-	if( me->current_move.ref == _POS_RELATIVE )
+	if( movement_insert_slot->ref == _POS_RELATIVE )
 	{
-		for( uint8_t i = 0; i < me->current_move.num_pts; i++ )
+		for( uint8_t i = 0; i < movement_insert_slot->num_pts; i++ )
 		{
-            me->current_move.points[i].x += me->effector_position.x;
-            me->current_move.points[i].y += me->effector_position.y;
-            me->current_move.points[i].z += me->effector_position.z;
+            movement_insert_slot->points[i].x += me->effector_position.x;
+            movement_insert_slot->points[i].y += me->effector_position.y;
+            movement_insert_slot->points[i].z += me->effector_position.z;
 		}
 	}
 
 	// A transit move is from current position to point 1, so overwrite 0 with current position, and then reuse a normal line movement
-	if( me->current_move.type == _POINT_TRANSIT)
+	if( movement_insert_slot->type == _POINT_TRANSIT)
 	{
-		if(me->current_move.num_pts == 1)
+		if(movement_insert_slot->num_pts == 1)
 		{
-			me->current_move.points[1].x = me->current_move.points[0].x;
-			me->current_move.points[1].y = me->current_move.points[0].y;
-			me->current_move.points[1].z = me->current_move.points[0].z;
-			me->current_move.num_pts = 2;
+			movement_insert_slot->points[1].x = movement_insert_slot->points[0].x;
+			movement_insert_slot->points[1].y = movement_insert_slot->points[0].y;
+			movement_insert_slot->points[1].z = movement_insert_slot->points[0].z;
+			movement_insert_slot->num_pts = 2;
 		}
 
-		me->current_move.points[0].x = me->effector_position.x;
-		me->current_move.points[0].y = me->effector_position.y;
-		me->current_move.points[0].z = me->effector_position.z;
+		movement_insert_slot->points[0].x = me->effector_position.x;
+		movement_insert_slot->points[0].y = me->effector_position.y;
+		movement_insert_slot->points[0].z = me->effector_position.z;
 	}
-
-	planner.enable = true;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -106,6 +121,27 @@ PUBLIC bool
 path_interpolator_get_move_done( void )
 {
 	return ( planner.progress_percent >= 1.0f - FLT_EPSILON);
+}
+
+/* -------------------------------------------------------------------------- */
+
+PRIVATE void
+path_interpolator_calculate_percentage( uint16_t move_duration )
+{
+    MotionPlanner_t *me = &planner;
+
+    // calculate current target completion based on time elapsed
+    // time remaining is the allotted duration - time used (start to now), divide by the duration to get 0.0->1.0 progress
+    uint32_t time_used = hal_systick_get_ms() - me->movement_started;
+
+    if( move_duration )
+    {
+        me->progress_percent = (float)( time_used ) / move_duration;
+    }
+    else
+    {
+        me->progress_percent = 0;
+    }
 }
 
 /* -------------------------------------------------------------------------- */
@@ -136,6 +172,10 @@ path_interpolator_stop(void )
 
     // Request that the statemachine return to "OFF"
     me->enable = false;
+
+    // Wipe out the moves currently loaded into the queue
+    memset(&me->move_a, 0, sizeof(Movement_t));
+    memset( &me->move_b, 0, sizeof(Movement_t));
 }
 
 /* -------------------------------------------------------------------------- */
@@ -164,95 +204,137 @@ path_interpolator_process( void )
 
             STATE_TRANSITION_TEST
 
-			if(planner.enable)
+			if( planner.enable && me->move_a.duration )
 			{
-        		STATE_NEXT( PLANNER_ON );
+        		STATE_NEXT(PLANNER_EXECUTE_A );
 			}
 
             STATE_EXIT_ACTION
             STATE_END
             break;
 
-        case PLANNER_ON:
+        case PLANNER_EXECUTE_A:
             STATE_ENTRY_ACTION
-				me->movement_started = hal_systick_get_ms();
+                me->movement_started = hal_systick_get_ms();
+                me->movement_est_complete = me->movement_started + me->move_a.duration;
             	me->progress_percent = 0;
-
             	config_set_pathing_status(me->currentState);
-
             STATE_TRANSITION_TEST
+                path_interpolator_calculate_percentage( me->move_a.duration );
 
-				Movement_t *move = &me->current_move;
+                if( !planner.enable || !me->move_a.duration )
+                {
+                    STATE_NEXT( PLANNER_OFF );
+                }
+                else if( path_interpolator_get_move_done() )
+                {
+                    if( me->move_b.duration )
+                    {
+                        STATE_NEXT( PLANNER_EXECUTE_B );
+                    }
+                    else
+                    {
+                        STATE_NEXT( PLANNER_OFF );
+                    }
 
-            	CartesianPoint_t target 	= { 0, 0, 0 };	//target position in cartesian space
-            	JointAngles_t angle_target 	= { 0, 0, 0 };	//target motor shaft angle in degrees
-
-            	//calculate current target completion based on time
-            	// time remaining is the allotted duration - time used (start to now), divide by the duration to get 0.0->1.0 progress
-            	uint32_t time_used = hal_systick_get_ms() - me->movement_started;
-            	me->progress_percent = (float)( time_used ) / move->duration;
-
-            	if( me->progress_percent >= 1.0f - FLT_EPSILON || planner.enable == false )
-            	{
-            		//movement is complete, the planner can stop now
-            		STATE_NEXT( PLANNER_OFF );
                     eventPublish( EVENT_NEW( StateEvent, PATHING_COMPLETE ) );
                 }
-            	else
-            	{
-                	switch( move->type )
-                	{
-    					case _POINT_TRANSIT:
-                            cartesian_point_on_line(move->points, move->num_pts, me->progress_percent, &target);
-    			    		break;
-
-    					case _LINE:
-                            cartesian_point_on_line(move->points, move->num_pts, me->progress_percent, &target);
-    						break;
-
-    					case _CATMULL_SPLINE:
-                            cartesian_point_on_catmull_spline(move->points, move->num_pts, me->progress_percent,
-                                                              &target);
-    						break;
-
-    					case _BEZIER_QUADRATIC:
-                            cartesian_point_on_quadratic_bezier(move->points, move->num_pts, me->progress_percent,
-                                                                &target);
-    						break;
-
-    					case _BEZIER_CUBIC:
-                            cartesian_point_on_cubic_bezier(move->points, move->num_pts, me->progress_percent, &target);
-    						break;
-    					default:
-    						//TODO this should be considered a motion error
-
-    						break;
-                	}
-
-                	kinematics_point_to_angle( target, &angle_target );
-
-                	// Ask the motors to please move there
-                	servo_set_target_angle( _CLEARPATH_1, angle_target.a1 );
-                	servo_set_target_angle( _CLEARPATH_2, angle_target.a2 );
-                	servo_set_target_angle( _CLEARPATH_3, angle_target.a3 );
-
-                	//update the config/UI data based on these actions
-                	config_set_position( target.x, target.y, target.z );
-
-                	memcpy( &me->effector_position, &target, sizeof(CartesianPoint_t));
-
-                	config_set_movement_data( move->identifier, move->type, (uint8_t)(me->progress_percent*100) );
-
-            	}
+                else
+                {
+                    execute_move( &me->move_a, me->progress_percent );
+                }
 
             STATE_EXIT_ACTION
-				planner.enable = false;
-                memset( &me->current_move, 0, sizeof(CartesianPoint_t));
+                memset( &me->move_a, 0, sizeof(Movement_t));
+            STATE_END
+            break;
 
+        case PLANNER_EXECUTE_B:
+            STATE_ENTRY_ACTION
+                me->movement_started = hal_systick_get_ms();
+                me->movement_est_complete = me->movement_started + me->move_b.duration;
+                me->progress_percent = 0;
+                config_set_pathing_status(me->currentState);
+            STATE_TRANSITION_TEST
+                path_interpolator_calculate_percentage( me->move_b.duration );
+
+                if( !planner.enable || !me->move_b.duration )
+                {
+                    STATE_NEXT( PLANNER_OFF );
+                }
+                else if( path_interpolator_get_move_done() )
+                {
+                    if( me->move_a.duration )
+                    {
+                        STATE_NEXT( PLANNER_EXECUTE_A );
+                    }
+                    else
+                    {
+                        STATE_NEXT( PLANNER_OFF );
+                    }
+
+                    eventPublish( EVENT_NEW( StateEvent, PATHING_COMPLETE ) );
+                }
+                else
+                {
+                    execute_move( &me->move_b, me->progress_percent );
+                }
+
+            STATE_EXIT_ACTION
+                memset(&me->move_b, 0, sizeof(Movement_t));
             STATE_END
             break;
     }
 
+}
+
+PRIVATE void
+execute_move( Movement_t *move, float percentage )
+{
+    CartesianPoint_t target 	= { 0, 0, 0 };	//target position in cartesian space
+    JointAngles_t angle_target 	= { 0, 0, 0 };	//target motor shaft angle in degrees
+
+    switch( move->type )
+    {
+        case _POINT_TRANSIT:
+            cartesian_point_on_line(move->points, move->num_pts, percentage, &target);
+            break;
+
+        case _LINE:
+            cartesian_point_on_line(move->points, move->num_pts, percentage, &target);
+            break;
+
+        case _CATMULL_SPLINE:
+            cartesian_point_on_catmull_spline(move->points, move->num_pts, percentage,
+                                              &target);
+            break;
+
+        case _BEZIER_QUADRATIC:
+            cartesian_point_on_quadratic_bezier(move->points, move->num_pts, percentage,
+                                                &target);
+            break;
+
+        case _BEZIER_CUBIC:
+            cartesian_point_on_cubic_bezier(move->points, move->num_pts, percentage, &target);
+            break;
+        default:
+            //TODO this should be considered a motion error
+
+            break;
+    }
+
+    // Calculate a motor angle solution for the cartesian position
+    kinematics_point_to_angle( target, &angle_target );
+
+    // Ask the motors to please move there
+    servo_set_target_angle( _CLEARPATH_1, angle_target.a1 );
+    servo_set_target_angle( _CLEARPATH_2, angle_target.a2 );
+    servo_set_target_angle( _CLEARPATH_3, angle_target.a3 );
+
+    //update the config/UI data based on these actions
+    config_set_position( target.x, target.y, target.z );
+    memcpy( &planner.effector_position, &target, sizeof(CartesianPoint_t));
+    config_set_movement_data( move->identifier, move->type, (uint8_t)(percentage*100) );
 }
 
 /* ----- End ---------------------------------------------------------------- */
