9b277af519fd2def90571103d4a29d8e9dbfaab2
Michael Orenstein <michael@whiteshadows.me>
2021-12-21 21:31:54 +1030

    Additional components for ingestion UI

--- interface/src/application/pages/toolpaths/interface/SettingsInterface.tsx
+++ interface/src/application/pages/toolpaths/interface/SettingsInterface.tsx
@@ -72,6 +72,36 @@ interface SettingsInterfaceProps {
   setSettings: Updater<Settings>
 }
 
+function ParticleWaitDurationSlider(props: SettingsInterfaceProps) {
+  const [localStopDelay, setLocalStopDelay] = useState(
+    props.settings.objectSettings.particles.stopDelay ?? 0,
+  )
+
+  const updateStopDelay = useCallback(
+    delay => {
+      props.setSettings(settings => {
+        settings.objectSettings.particles.stopDelay = delay
+      })
+    },
+    [props.setSettings],
+  )
+
+  return (
+    <div style={{ marginLeft: 10, marginRight: 10 }}>
+      <Slider
+        min={0}
+        max={200}
+        stepSize={1}
+        labelStepSize={50}
+        value={localStopDelay}
+        labelRenderer={value => `${value}ms`}
+        onRelease={updateStopDelay}
+        onChange={setLocalStopDelay}
+      />
+    </div>
+  )
+}
+
 function MaxSpeedSlider(props: SettingsInterfaceProps) {
   const [localSpeed, setLocalSpeed] = useState(
     props.settings.optimisation.maxSpeed,
@@ -87,24 +117,68 @@ function MaxSpeedSlider(props: SettingsInterfaceProps) {
   )
 
   return (
-    <Slider
-      min={0}
-      max={300}
-      stepSize={1}
-      labelStepSize={50}
-      value={localSpeed}
-      labelRenderer={value => `${value}mm/s`}
-      onRelease={updateMaxSpeed}
-      onChange={setLocalSpeed}
-    />
+    <div style={{ marginLeft: 10, marginRight: 10 }}>
+      <Slider
+        min={0}
+        max={300}
+        stepSize={1}
+        labelStepSize={50}
+        value={Math.max(localSpeed, 1)}
+        labelRenderer={value => `${value}mm/s`}
+        onRelease={updateMaxSpeed}
+        onChange={setLocalSpeed}
+      />
+    </div>
+  )
+}
+
+function WaitAtStartDurationSlider(props: SettingsInterfaceProps) {
+  const [waitDuration, setWaitDuration] = useState(
+    props.settings.optimisation.waitAtStartDuration,
+  )
+
+  const updateWaitDuration = useCallback(
+    waitDuration => {
+      props.setSettings(settings => {
+        settings.optimisation.waitAtStartDuration = waitDuration
+      })
+    },
+    [props.setSettings],
+  )
+
+  return (
+    <div style={{ marginLeft: 10, marginRight: 10 }}>
+      <Slider
+        min={0}
+        max={5000}
+        stepSize={100}
+        labelStepSize={500}
+        value={waitDuration}
+        labelRenderer={value => `${Math.round((value / 1000) * 10) / 10}s`}
+        onRelease={updateWaitDuration}
+        onChange={setWaitDuration}
+      />
+    </div>
   )
 }
 
 export const SettingsInterface = (props: SettingsInterfaceProps) => {
   return (
     <Card>
+      <FormGroup label="Particle wait duration">
+        <ParticleWaitDurationSlider
+          settings={props.settings}
+          setSettings={props.setSettings}
+        />
+      </FormGroup>
       <FormGroup label="Maximum speed">
         <MaxSpeedSlider
+          settings={props.settings}
+          setSettings={props.setSettings}
+        />
+      </FormGroup>
+      <FormGroup label="Wait at start duration">
+        <WaitAtStartDurationSlider
           settings={props.settings}
           setSettings={props.setSettings}
         />
