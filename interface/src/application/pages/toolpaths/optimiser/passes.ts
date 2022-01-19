8d39fcecb5952dfc07daa66c5f09fa22a8ba5198
Michael Orenstein <michael@whiteshadows.me>
2022-01-19 12:08:23 +1030

    Fixed bugs in TSP solver

--- interface/src/application/pages/toolpaths/optimiser/passes.ts
+++ interface/src/application/pages/toolpaths/optimiser/passes.ts
@@ -227,7 +227,7 @@ export function sparseToCost(movements: Movement[]): number {
   return d2Total
 }
 
-function swap(array: any[], a: number, b: number) {
+export function swap(array: any[], a: number, b: number) {
   const temp = array[a]
   array[a] = array[b]
   array[b] = temp
@@ -268,7 +268,7 @@ function optimiseByCache(sparseBag: Movement[], orderingCache: OrderingCache) {
   return movements
 }
 
-function optimiseBySearch(sparseBag: Movement[]) {
+export function optimiseBySearch(sparseBag: Movement[]) {
   if (sparseBag.length < 2) {
     return sparseBag
   }
@@ -277,7 +277,7 @@ function optimiseBySearch(sparseBag: Movement[]) {
 
   // Pick a random movement to start at, remove it from the array
   let previousMovement = toOrder.splice(
-    Math.floor(Math.random() * toOrder.length),
+    0, // start at 0
     1, // grab one
   )[0] // splice returns an array of the deleted items, index 0 is our starting point
   const nnOrdering = [previousMovement]
@@ -330,7 +330,29 @@ function optimiseBySearch(sparseBag: Movement[]) {
   return nnOrdering
 }
 
-function optimise2Opt(sparseBag: Movement[], timeLimit = 0) {
+const n = false
+const f = true
+
+function d(
+  first: Movement,
+  firstFlipped: boolean,
+  second: Movement,
+  secondFlipped: boolean,
+) {
+  if (!firstFlipped && !secondFlipped) {
+    return first.getEnd().distanceTo(second.getStart())
+  } else if (firstFlipped && !secondFlipped) {
+    return first.getStart().distanceTo(second.getStart())
+  } else if (!firstFlipped && secondFlipped) {
+    return first.getEnd().distanceTo(second.getEnd())
+  } else if (firstFlipped && secondFlipped) {
+    return first.getStart().distanceTo(second.getEnd())
+  }
+
+  throw new Error(`Unreachable`)
+}
+
+export function optimise2Opt(sparseBag: Movement[], timeLimit = 0) {
   const ordering: Movement[] = sparseBag.slice()
 
   let improved = true // Start iterating
@@ -343,99 +365,242 @@ function optimise2Opt(sparseBag: Movement[], timeLimit = 0) {
     iteration++
 
     if (timeLimit > 0 && Date.now() - start > timeLimit) {
-      return { ordering, iteration, completed: false }
+      return {
+        ordering,
+        iteration,
+        completed: false,
+        time: Date.now() - start,
+      }
+    }
+
+    // The main iteration loop can't flip or swap the ends, so do that up here
+    {
+      // Test flipping the start
+      const A = ordering[0]
+      const B = ordering[1]
+
+      const dAnB = A.getEnd().distanceTo(B.getStart())
+      const dAfB = A.getStart().distanceTo(B.getEnd())
+
+      if (dAfB < dAnB) {
+        A.flip()
+
+        const delta = dAnB - dAfB
+
+        cost = cost - delta
+        improved = true
+      }
+    }
+    {
+      // Test flipping the end
+      const E = ordering[ordering.length - 2]
+      const F = ordering[ordering.length - 1]
+
+      const dEFn = E.getEnd().distanceTo(F.getStart())
+      const dEFf = E.getEnd().distanceTo(F.getEnd())
+
+      if (dEFf < dEFn) {
+        F.flip()
+
+        const delta = dEFn - dEFf
+
+        cost = cost - delta
+        improved = true
+      }
+    }
+    {
+      // Test a left rotation
+      const A = ordering[0]
+      const B = ordering[1]
+      const C = ordering[ordering.length - 2]
+      const D = ordering[ordering.length - 1]
+
+      // A B ... C D
+      // B ... C D A
+      // destroys the AB link, adds the DA link
+
+      const dAnBn = d(A, n, B, n)
+      const dDnAn = d(D, n, A, n)
+
+      if (dDnAn < dAnBn) {
+        ordering.push(ordering.shift()!)
+
+        const delta = dAnBn - dDnAn
+
+        cost = cost - delta
+        improved = true
+      }
+    }
+    {
+      // Test a right rotation
+      const A = ordering[0]
+      const B = ordering[1]
+      const C = ordering[ordering.length - 2]
+      const D = ordering[ordering.length - 1]
+
+      // A B ... C D
+      // D A B ... C
+      // destroys the CD link, adds the DA link
+
+      const dCnDn = d(C, n, D, n)
+      const dDnAn = d(D, n, A, n)
+
+      if (dDnAn < dCnDn) {
+        ordering.unshift(ordering.pop()!)
+
+        const delta = dCnDn - dDnAn
+
+        cost = cost - delta
+        improved = true
+      }
+    }
+    {
+      // TODO: Test a flip
     }
 
     // Start and end points are fixed with this algorithm
-    iteration: for (let i = 1; i < ordering.length - 2; i++) {
-      for (let j = i + 1; j < ordering.length - 1; j++) {
-        const A = ordering[i - 1]
-        const B = ordering[i]
-        const C = ordering[i + 1]
-        const D = ordering[j - 1]
-        const E = ordering[j]
-        const F = ordering[j + 1]
+    iteration: for (let b = 1; b < ordering.length - 2; b++) {
+      for (let e = b + 1; e < ordering.length - 1; e++) {
+        // If they're next to each other, it's a special case
+        if (e === b + 1) {
+          const A = ordering[b - 1]
+          const B = ordering[b]
+          const C = ordering[e]
+          const D = ordering[e + 1]
+
+          const current = d(A, n, B, n) + d(B, n, C, n) + d(C, n, D, n)
+          const flipB = d(A, n, B, f) + d(B, f, C, n) + d(C, n, D, n)
+          const flipC = d(A, n, B, n) + d(B, n, C, f) + d(C, f, D, n)
+          const flipBC = d(A, n, B, f) + d(B, f, C, f) + d(C, f, D, n)
+          const swapBC = d(A, n, C, n) + d(C, n, B, n) + d(B, n, D, n)
+          const flipBSwapBC = d(A, n, C, n) + d(C, n, B, f) + d(B, f, D, n)
+          const flipCSwapBC = d(A, n, C, f) + d(C, f, B, n) + d(B, n, D, n)
+          const flipBCSwapBC = d(A, n, C, f) + d(C, f, B, f) + d(B, f, D, n)
+
+          // Find the winner
+          const smallest = Math.min(
+            current,
+            flipB,
+            flipC,
+            flipBC,
+            swapBC,
+            flipBSwapBC,
+            flipCSwapBC,
+            flipBCSwapBC,
+          )
+
+          // update the cost
+          const delta = current - smallest
+          cost = cost - delta
+
+          // Do the operations of the winner
+
+          if (smallest === current) {
+            // No improvement
+          } else if (smallest === flipB) {
+            ordering[b].flip()
+            improved = true
+          } else if (smallest === flipC) {
+            ordering[e].flip()
+            improved = true
+          } else if (smallest === flipBC) {
+            ordering[b].flip()
+            ordering[e].flip()
+            improved = true
+          } else if (smallest === swapBC) {
+            swap(ordering, b, e)
+            improved = true
+          } else if (smallest === flipBSwapBC) {
+            ordering[b].flip()
+            swap(ordering, b, e)
+            improved = true
+          } else if (smallest === flipCSwapBC) {
+            ordering[e].flip()
+            swap(ordering, b, e)
+            improved = true
+          } else if (smallest === flipBCSwapBC) {
+            ordering[b].flip()
+            ordering[e].flip()
+            swap(ordering, b, e)
+            improved = true
+          }
+
+          continue
+        }
+
+        // The segments are not overlapping
+        const A = ordering[b - 1]
+        const B = ordering[b]
+        const C = ordering[b + 1]
+        const D = ordering[e - 1]
+        const E = ordering[e]
+        const F = ordering[e + 1]
 
         // Calculate the distances of the different segments, flipped or swapped
-        const dABnC = A.getEnd().distanceTo(B.getStart()) + B.getEnd().distanceTo(C.getStart()) // prettier-ignore
-        const dDEnF = D.getEnd().distanceTo(E.getStart()) + E.getEnd().distanceTo(F.getStart()) // prettier-ignore
-
-        const dAEnC = A.getEnd().distanceTo(E.getStart()) + E.getEnd().distanceTo(C.getStart()) // prettier-ignore
-        const dDBnF = D.getEnd().distanceTo(B.getStart()) + B.getEnd().distanceTo(F.getStart()) // prettier-ignore
-
-        const dABfC = A.getEnd().distanceTo(B.getEnd()) + B.getStart().distanceTo(C.getStart()) // prettier-ignore
-        const dDEfF = D.getEnd().distanceTo(E.getEnd()) + E.getStart().distanceTo(F.getStart()) // prettier-ignore
-
-        const dAEfC = A.getEnd().distanceTo(E.getEnd()) + E.getStart().distanceTo(C.getStart()) // prettier-ignore
-        const dDBfF = D.getEnd().distanceTo(B.getEnd()) + B.getStart().distanceTo(F.getStart()) // prettier-ignore
-
-        // Calculate cost deltas
-        const current = dABnC + dDEnF
-        const flipI = dABfC + dDEnF
-        const flipJ = dABnC + dDEfF
-        const flipIJ = dABfC + dDEfF
-        const swappedIJ = dAEnC + dDBnF
-        const flipISwapIJ = dAEnC + dDBfF
-        const flipJSwapIJ = dAEfC + dDBnF
-        const flipIJSwapIJ = dAEfC + dDBfF
+        const current = d(A, n, B, n) + d(B, n, C, n) + d(D, n, E, n) + d(E, n, F, n) // prettier-ignore
+        const flipB = d(A, n, B, f) + d(B, f, C, n) + d(D, n, E, n) + d(E, n, F, n) // prettier-ignore
+        const flipE = d(A, n, B, n) + d(B, n, C, n) + d(D, n, E, f) + d(E, f, F, n) // prettier-ignore
+        const flipBE = d(A, n, B, f) + d(B, f, C, n) + d(D, n, E, f) + d(E, f, F, n) // prettier-ignore
+        const swapBE = d(A, n, E, n) + d(E, n, C, n) + d(D, n, B, n) + d(B, n, F, n) // prettier-ignore
+        const flipBSwapBE = d(A, n, E, n) + d(E, n, C, n) + d(D, n, B, f) + d(B, f, F, n) // prettier-ignore
+        const flipESwapBE = d(A, n, E, f) + d(E, f, C, n) + d(D, n, B, n) + d(B, n, F, n) // prettier-ignore
+        const flipBESwapBE = d(A, n, E, f) + d(E, f, C, n) + d(D, n, B, f) + d(B, f, F, n) // prettier-ignore
 
         // Find the winner
         const smallest = Math.min(
           current,
-          flipI,
-          flipJ,
-          flipIJ,
-          swappedIJ,
-          flipISwapIJ,
-          flipJSwapIJ,
-          flipIJSwapIJ,
+          flipB,
+          flipE,
+          flipBE,
+          swapBE,
+          flipBSwapBE,
+          flipESwapBE,
+          flipBESwapBE,
         )
+
+        // update the cost
+        const delta = current - smallest
+        cost = cost - delta
 
         // Do the operations of the winner
         if (smallest === current) {
           // No improvement
-          // continue // try the next one
-        } else if (smallest === flipI) {
-          ordering[i].flip()
-          cost = cost - current + flipI
+        } else if (smallest === flipB) {
+          ordering[b].flip()
           improved = true
-        } else if (smallest === flipJ) {
-          ordering[j].flip()
-          cost = cost - current + flipJ
+        } else if (smallest === flipE) {
+          ordering[e].flip()
           improved = true
-        } else if (smallest === flipIJ) {
-          ordering[i].flip()
-          ordering[j].flip()
-          cost = cost - current + flipIJ
+        } else if (smallest === flipBE) {
+          ordering[b].flip()
+          ordering[e].flip()
           improved = true
-        } else if (smallest === swappedIJ) {
-          swap(ordering, i, j)
-          cost = cost - current + swappedIJ
+        } else if (smallest === swapBE) {
+          swap(ordering, b, e)
           improved = true
-        } else if (smallest === flipISwapIJ) {
-          ordering[i].flip()
-          swap(ordering, i, j)
-          cost = cost - current + flipISwapIJ
+        } else if (smallest === flipBSwapBE) {
+          ordering[b].flip()
+          swap(ordering, b, e)
           improved = true
-        } else if (smallest === flipJSwapIJ) {
-          ordering[j].flip()
-          swap(ordering, i, j)
-          cost = cost - current + flipJSwapIJ
+        } else if (smallest === flipESwapBE) {
+          ordering[e].flip()
+          swap(ordering, b, e)
           improved = true
-        } else if (smallest === flipIJSwapIJ) {
-          ordering[i].flip()
-          ordering[j].flip()
-          swap(ordering, i, j)
-          cost = cost - current + flipIJSwapIJ
+        } else if (smallest === flipBESwapBE) {
+          ordering[b].flip()
+          ordering[e].flip()
+          swap(ordering, b, e)
           improved = true
         }
 
-        // continue iteration
+        if (cost < 0) {
+          throw new Error(`TSP score went < 0`)
+        }
       }
     }
   }
 
-  return { ordering, iteration, completed: true }
+  return { ordering, iteration, completed: true, time: Date.now() - start }
 }
 
 /**
@@ -490,7 +655,7 @@ export async function optimise(
     return
   }
 
-  let ordering = sparseBag
+  let ordering = optimiseBySearch(sparseBag)
 
   let iterations = 0
 
@@ -500,12 +665,6 @@ export async function optimise(
     const nextPass = optimise2Opt(ordering, 1000)
 
     iterations += nextPass.iteration
-
-    console.log(
-      `did ${nextPass.iteration} that second, total of ${iterations}, ${
-        iterations / ((Date.now() - startedOptimisation) / 1000)
-      } iter/s`,
-    )
 
     const currentDense = sparseToDense(nextPass.ordering, settings)
     const curentDuration = getTotalDuration(currentDense)
