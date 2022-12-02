184fe7102ed079c20f26589e2e461db8383f8235
Scott Rapson <scottapotamas@gmail.com>
2022-12-02 16:03:41 +1030

    Minor formatting

--- interface/src/application/pages/DevicePages/Controls/ControlPalette/ManualJogPalette.tsx
+++ interface/src/application/pages/DevicePages/Controls/ControlPalette/ManualJogPalette.tsx
@@ -4,7 +4,11 @@ import { Icon, MultiSlider, NumericInput, Tag, Intent } from '@blueprintjs/core'
 import { Composition, Only } from 'atomic-layout'
 
 import { Button, NumberInput } from '@electricui/components-desktop-blueprint'
-import { MSGID } from 'src/application/typedState'
+import {
+  MovementMoveReference,
+  MovementMoveType,
+  MSGID,
+} from 'src/application/typedState'
 
 const JogButtonAreas = `
 XP YP ZP
@@ -18,124 +22,128 @@ interface JogPaletteProps {
 
 const JogPalette = (props: JogPaletteProps) => {
   return (
-    // X axis is red, Y axis is green, z axis is blue
-    // This matches the Blender color associations
-    <Composition areas={JogButtonAreas} gap={5} templateCols="auto auto 1fr">
-      {({ XP, YP, ZP, XM, YM, ZM }) => (
-        <React.Fragment>
-          <XP>
-            <Button
-              large
-              outlined
-              intent={Intent.DANGER}
-              writer={state => {
-                state[MSGID.QUEUE_ADD_MOVE] = {
-                  type: 0,
-                  reference: 1,
-                  sync_offset: 0,
-                  duration: props.time,
-                  points: [[props.distance, 0, 0]],
-                }
-              }}
-            >
-              <Icon icon="arrow-top-left" iconSize={50} />
-            </Button>
-          </XP>
-          <YP>
-            <Button
-              large
-              outlined
-              intent={Intent.SUCCESS}
-              writer={state => {
-                state[MSGID.QUEUE_ADD_MOVE] = {
-                  type: 0,
-                  reference: 1,
-                  sync_offset: 0,
-                  duration: props.time,
-                  points: [[0, props.distance, 0]],
-                }
-              }}
-            >
-              <Icon icon="arrow-top-right" iconSize={50} />
-            </Button>
-          </YP>
-          <XM>
-            <Button
-              large
-              outlined
-              intent={Intent.DANGER}
-              writer={state => {
-                state[MSGID.QUEUE_ADD_MOVE] = {
-                  type: 0,
-                  reference: 1,
-                  sync_offset: 0,
-                  duration: props.time,
-                  points: [[-props.distance, 0, 0]],
-                }
-              }}
-            >
-              <Icon icon="arrow-bottom-right" iconSize={50} />
-            </Button>
-          </XM>
-          <YM>
-            <Button
-              large
-              outlined
-              intent={Intent.SUCCESS}
-              writer={state => {
-                state[MSGID.QUEUE_ADD_MOVE] = {
-                  type: 0,
-                  reference: 1,
-                  sync_offset: 0,
-                  duration: props.time,
-                  points: [[0, -props.distance, 0]],
-                }
-              }}
-            >
-              <Icon icon="arrow-bottom-left" iconSize={50} />
-            </Button>
-          </YM>
-          <ZP>
-            <Button
-              large
-              fill
-              outlined
-              intent={Intent.PRIMARY}
-              writer={state => {
-                state[MSGID.QUEUE_ADD_MOVE] = {
-                  type: 0,
-                  reference: 1,
-                  sync_offset: 0,
-                  duration: props.time,
-                  points: [[0, 0, props.distance]],
-                }
-              }}
-            >
-              <Icon icon="arrow-up" iconSize={50} />
-            </Button>
-          </ZP>
-          <ZM>
-            <Button
-              large
-              fill
-              outlined
-              intent={Intent.PRIMARY}
-              writer={state => {
-                state[MSGID.QUEUE_ADD_MOVE] = {
-                  type: 0,
-                  reference: 1,
-                  sync_offset: 0,
-                  duration: props.time,
-                  points: [[0, 0, -props.distance]],
-                }
-              }}
-            >
-              <Icon icon="arrow-down" iconSize={50} />
-            </Button>
-          </ZM>
-        </React.Fragment>
-      )}
-    </Composition>
+    <>
+      {/* 
+        X axis is red, Y axis is green, z axis is blue
+        This matches the Blender color associations 
+      */}
+      <Composition areas={JogButtonAreas} gap={5} templateCols="auto auto 1fr">
+        {({ XP, YP, ZP, XM, YM, ZM }) => (
+          <React.Fragment>
+            <XP>
+              <Button
+                large
+                outlined
+                intent={Intent.DANGER}
+                writer={state => {
+                  state[MSGID.QUEUE_ADD_MOVE] = {
+                    type: 0,
+                    reference: 1,
+                    sync_offset: 0,
+                    duration: props.time,
+                    points: [[props.distance, 0, 0]],
+                  }
+                }}
+              >
+                <Icon icon="arrow-top-left" iconSize={50} />
+              </Button>
+            </XP>
+            <YP>
+              <Button
+                large
+                outlined
+                intent={Intent.SUCCESS}
+                writer={state => {
+                  state[MSGID.QUEUE_ADD_MOVE] = {
+                    type: 0,
+                    reference: 1,
+                    sync_offset: 0,
+                    duration: props.time,
+                    points: [[0, props.distance, 0]],
+                  }
+                }}
+              >
+                <Icon icon="arrow-top-right" iconSize={50} />
+              </Button>
+            </YP>
+            <XM>
+              <Button
+                large
+                outlined
+                intent={Intent.DANGER}
+                writer={state => {
+                  state[MSGID.QUEUE_ADD_MOVE] = {
+                    type: 0,
+                    reference: 1,
+                    sync_offset: 0,
+                    duration: props.time,
+                    points: [[-props.distance, 0, 0]],
+                  }
+                }}
+              >
+                <Icon icon="arrow-bottom-right" iconSize={50} />
+              </Button>
+            </XM>
+            <YM>
+              <Button
+                large
+                outlined
+                intent={Intent.SUCCESS}
+                writer={state => {
+                  state[MSGID.QUEUE_ADD_MOVE] = {
+                    type: 0,
+                    reference: 1,
+                    sync_offset: 0,
+                    duration: props.time,
+                    points: [[0, -props.distance, 0]],
+                  }
+                }}
+              >
+                <Icon icon="arrow-bottom-left" iconSize={50} />
+              </Button>
+            </YM>
+            <ZP>
+              <Button
+                large
+                fill
+                outlined
+                intent={Intent.PRIMARY}
+                writer={state => {
+                  state[MSGID.QUEUE_ADD_MOVE] = {
+                    type: 0,
+                    reference: 1,
+                    sync_offset: 0,
+                    duration: props.time,
+                    points: [[0, 0, props.distance]],
+                  }
+                }}
+              >
+                <Icon icon="arrow-up" iconSize={50} />
+              </Button>
+            </ZP>
+            <ZM>
+              <Button
+                large
+                fill
+                outlined
+                intent={Intent.PRIMARY}
+                writer={state => {
+                  state[MSGID.QUEUE_ADD_MOVE] = {
+                    type: 0,
+                    reference: 1,
+                    sync_offset: 0,
+                    duration: props.time,
+                    points: [[0, 0, -props.distance]],
+                  }
+                }}
+              >
+                <Icon icon="arrow-down" iconSize={50} />
+              </Button>
+            </ZM>
+          </React.Fragment>
+        )}
+      </Composition>
+    </>
   )
 }
 
