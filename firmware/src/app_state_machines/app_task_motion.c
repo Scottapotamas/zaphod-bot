7685983b373a844b2a037375ebfcfb73ba9dc7ab
Scott Rapson <scottapotamas@gmail.com>
2023-01-17 00:46:33 +1030

    Revert motion handler estop top level event handling
    Wasn't correctly transitioning into recovery state.

--- firmware/src/app_state_machines/app_task_motion.c
+++ firmware/src/app_state_machines/app_task_motion.c
@@ -187,6 +187,10 @@ PRIVATE STATE AppTaskMotion_home( AppTaskMotion *me, const StateEvent *e )
             AppTaskMotion_add_event_to_queue( me, e );
             return 0;
 
+        case MOTION_EMERGENCY:
+            STATE_TRAN( AppTaskMotion_recovery );
+            return 0;
+
         case STATE_EXIT_SIGNAL:
             eventTimerStopIfActive( &me->timer1 );
 
@@ -248,6 +252,10 @@ PRIVATE STATE AppTaskMotion_inactive( AppTaskMotion *me, const StateEvent *e )
 
             return 0;
         }
+
+        case MOTION_EMERGENCY:
+            STATE_TRAN( AppTaskMotion_recovery );
+            return 0;
 
         case STATE_EXIT_SIGNAL:
             eventTimerStopIfActive( &me->timer1 );
@@ -321,6 +329,10 @@ PRIVATE STATE AppTaskMotion_active( AppTaskMotion *me, const StateEvent *e )
         case MOTION_QUEUE_CLEAR:
             AppTaskMotion_clear_queue( me );
             STATE_TRAN( AppTaskMotion_inactive );
+            return 0;
+
+        case MOTION_EMERGENCY:
+            STATE_TRAN( AppTaskMotion_recovery );
             return 0;
 
         case STATE_EXIT_SIGNAL:
