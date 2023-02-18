40153a028113eb15b5702f9e5b22195b7f9a6dcf
Scott Rapson <scottapotamas@gmail.com>
2023-02-19 10:09:25 +1030

    Extract HSI conversion function out of led_interpolator module

--- firmware/src/drivers/led_interpolator.c
+++ firmware/src/drivers/led_interpolator.c
@@ -67,12 +67,6 @@ PRIVATE FadeSolution_t
 hsi_lerp_linear( HSIColour_t p[], size_t points, float pos_weight, HSIColour_t *output );
 
 PRIVATE void
-hsi_to_rgb( float h, float s, float i, float *r, float *g, float *b );
-
-PRIVATE float
-hue_to_channel( float p, float q, float t );
-
-PRIVATE void
 led_interpolator_notify_animation_started( uint32_t fade_id );
 
 PRIVATE void
@@ -454,76 +448,6 @@ hsi_lerp_linear( HSIColour_t p[], size_t points, float pos_weight, HSIColour_t *
 }
 
 /* -------------------------------------------------------------------------- */
-
-/* Converts a Hue/Saturation/Intensity colour to a Red/Green/Blue one
- * Input HSI are [0, 1]
- * Output RGB are [0, 1]
- */
-void hsi_to_rgb( float h, float s, float i, float *r, float *g, float *b )
-{
-    REQUIRE( r );
-    REQUIRE( g );
-    REQUIRE( b );
-
-    float q;
-    float p;
-
-    // no saturation --> white/achromatic at the given intensity level
-    if( s <= 0.0f )
-    {
-        *r = i;
-        *g = i;
-        *b = i;
-    }
-    else
-    {
-        if( i < 0.5 )
-        {
-            q = i * ( 1.0f + s );
-        }
-        else
-        {
-            q = i + s - i * s;
-        }
-
-        p = 2 * i - q;
-
-        *r = hue_to_channel( p, q, h + 1.0f / 3.0f );
-        *g = hue_to_channel( p, q, h );
-        *b = hue_to_channel( p, q, h - 1.0f / 3.0f );
-    }
-}
-
-// Helper for HSI/RGB conversions
-float hue_to_channel( float p, float q, float t )
-{
-    if( t < 0.0f )
-    {
-        t += 1.0f;
-    }
-
-    if( t > 1.0f )
-    {
-        t -= 1.0f;
-    }
-
-    if( t < 1.0f / 6.0f )
-    {
-        return p + ( q - p ) * 6.0f * t;
-    }
-
-    if( t < 1.0f / 2.0f )
-    {
-        return q;
-    }
-
-    if( t < 2.0f / 3.0f )
-    {
-        return p + ( q - p ) * ( 2.0f / 3.0f - t ) * 6.0f;
-    }
-
-    return p;
-}
 
 PRIVATE void
 led_interpolator_notify_animation_started( uint32_t fade_id )
---
+++ firmware/src/drivers/led_types.c
@@ -0,0 +1,95 @@
+/* ----- System Includes ---------------------------------------------------- */
+
+
+/* ----- Local Includes ----------------------------------------------------- */
+
+#include "app_times.h"
+#include "led_types.h"
+#include "qassert.h"
+
+/* ----- Defines ------------------------------------------------------------ */
+
+DEFINE_THIS_FILE; /* Used for ASSERT checks to define __FILE__ only once */
+
+/* -------------------------------------------------------------------------- */
+
+PRIVATE float
+hue_to_channel( float p, float q, float t );
+
+/* ----- Public Functions --------------------------------------------------- */
+
+/* Converts a Hue/Saturation/Intensity colour to a Red/Green/Blue one
+ * Input HSI are [0, 1]
+ * Output RGB are [0, 1]
+ */
+PUBLIC void
+hsi_to_rgb( float h, float s, float i, float *r, float *g, float *b )
+{
+    REQUIRE( r );
+    REQUIRE( g );
+    REQUIRE( b );
+
+    float q;
+    float p;
+
+    // no saturation --> white/achromatic at the given intensity level
+    if( s <= 0.0f )
+    {
+        *r = i;
+        *g = i;
+        *b = i;
+    }
+    else
+    {
+        if( i < 0.5 )
+        {
+            q = i * ( 1.0f + s );
+        }
+        else
+        {
+            q = i + s - i * s;
+        }
+
+        p = 2 * i - q;
+
+        *r = hue_to_channel( p, q, h + 1.0f / 3.0f );
+        *g = hue_to_channel( p, q, h );
+        *b = hue_to_channel( p, q, h - 1.0f / 3.0f );
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+
+// Helper for HSI/RGB conversions
+PRIVATE float
+hue_to_channel( float p, float q, float t )
+{
+    if( t < 0.0f )
+    {
+        t += 1.0f;
+    }
+
+    if( t > 1.0f )
+    {
+        t -= 1.0f;
+    }
+
+    if( t < 1.0f / 6.0f )
+    {
+        return p + ( q - p ) * 6.0f * t;
+    }
+
+    if( t < 1.0f / 2.0f )
+    {
+        return q;
+    }
+
+    if( t < 2.0f / 3.0f )
+    {
+        return p + ( q - p ) * ( 2.0f / 3.0f - t ) * 6.0f;
+    }
+
+    return p;
+}
+
+/* ----- End ---------------------------------------------------------------- */
