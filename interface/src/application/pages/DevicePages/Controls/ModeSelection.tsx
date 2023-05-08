557d49f056af308a86ae7aab21b8cedc2e060fe3
Scott Rapson <scottapotamas@gmail.com>
2023-05-08 11:20:54 +0930

    Refactor mode selection UI buttons

--- interface/src/application/pages/DevicePages/Controls/ModeSelection.tsx
+++ interface/src/application/pages/DevicePages/Controls/ModeSelection.tsx
@@ -1,130 +1,68 @@
 import React from 'react'
 
-import { Dropdown, Button } from '@electricui/components-desktop-blueprint'
-import { MSGID, CONTROL_MODES } from 'src/application/typedState'
-import { Icon, Colors, NonIdealState, ButtonGroup, Intent } from '@blueprintjs/core'
+import { Button } from '@electricui/components-desktop-blueprint'
+import { Intent, IconName } from '@blueprintjs/core'
 import { useHardwareState } from '@electricui/components-core'
-
 import { Composition } from 'atomic-layout'
 
-export const ModeSelection = () => {
-  const control_mode = useHardwareState(state => state[MSGID.SUPERVISOR].mode)
+import { MSGID, CONTROL_MODES } from 'src/application/typedState'
 
-  const largeButtons: boolean = false
+interface ModeButton {
+  label: string
+  icon: IconName  // TODO: consider using the properly typed icons
+  value: CONTROL_MODES
+}
+
+// Each of the 4 user buttons to change mode
+const buttons: ModeButton[] = [
+  { label: 'Jog', icon: 'move', value: CONTROL_MODES.MANUAL },
+  { label: 'Track', icon: 'flow-end', value: CONTROL_MODES.TRACK },
+  { label: 'Demo', icon: 'projects', value: CONTROL_MODES.DEMO },
+  { label: 'Blender', icon: 'document-open', value: CONTROL_MODES.EVENT },
+]
+
+interface ModeButtonProps {
+  config: ModeButton
+  activeState: string
+}
+
+const ModeButtonComponent = (props: ModeButtonProps) => {
+  return (
+    <Button
+      active={props.activeState == CONTROL_MODES[props.config.value]}
+      outlined
+      intent={
+        props.activeState == CONTROL_MODES[props.config.value]
+          ? Intent.PRIMARY
+          : Intent.NONE
+      }
+      icon={props.config.icon}
+      writer={state => {
+        state[MSGID.MODE_REQUEST] = props.config.value
+      }}
+    >
+      {props.config.label.toUpperCase()}
+    </Button>
+  )
+}
+
+// The 4-buttons in a formatted row
+export const ModeSelection = () => {
+  const control_mode =
+    useHardwareState(state => state[MSGID.SUPERVISOR].mode) ??
+    CONTROL_MODES[CONTROL_MODES.NONE]
 
   return (
     <React.Fragment>
       <Composition templateCols="1fr 1fr 1fr 1fr" gapCol={10}>
-      <Button
-          active={control_mode == CONTROL_MODES[CONTROL_MODES.MANUAL]}
-          outlined
-          intent={control_mode == CONTROL_MODES[CONTROL_MODES.MANUAL]? Intent.PRIMARY: Intent.NONE}
-          icon="move"
-          writer={state => {
-            state[MSGID.MODE_REQUEST] = 1
-          }}
-        >
-          JOG
-        </Button>
-        <Button
-          active={control_mode == CONTROL_MODES[CONTROL_MODES.TRACK]}
-          outlined 
-          intent={control_mode == CONTROL_MODES[CONTROL_MODES.TRACK]? Intent.PRIMARY: Intent.NONE}
-           
-          icon="flow-end"
-          writer={state => {
-            state[MSGID.MODE_REQUEST] = 3
-          }}
-        >
-          TRACK
-        </Button>
-        <Button
-          active={control_mode == CONTROL_MODES[CONTROL_MODES.DEMO]}
-          outlined 
-          intent={control_mode == CONTROL_MODES[CONTROL_MODES.DEMO]? Intent.PRIMARY: Intent.NONE}
-
-          icon="projects"
-          writer={state => {
-            state[MSGID.MODE_REQUEST] = 4
-          }}
-        >
-          DEMO
-        </Button>
-        <Button
-          active={control_mode == CONTROL_MODES[CONTROL_MODES.EVENT]}
-          outlined 
-          intent={control_mode == CONTROL_MODES[CONTROL_MODES.EVENT]? Intent.PRIMARY: Intent.NONE}
-
-          icon="document-open"
-          writer={state => {
-            state[MSGID.MODE_REQUEST] = 2
-          }}
-        >
-          BLENDER
-        </Button>
+        {buttons.map(buttonConfig => (
+          <ModeButtonComponent
+            config={buttonConfig}
+            activeState={control_mode}
+            key={buttonConfig.value}
+          />
+        ))}
       </Composition>
-    </React.Fragment>
-  )
-}
-
-export const ModeSelectionDropdown = () => {
-  const control_mode = useHardwareState(state => state[MSGID.SUPERVISOR].mode)
-
-  return (
-    <React.Fragment>
-      <Dropdown
-        accessor={state => state[MSGID.MODE_REQUEST]}
-        writer={(state, value) => {
-          state[MSGID.MODE_REQUEST] = value
-        }}
-        placeholder={selectedOption =>
-          selectedOption ? `Mode: ${selectedOption.text}` : 'Select Mode'
-        }
-      >
-        <Dropdown.Option
-          value={1}
-          text="Manual"
-          labelElement={
-            <>
-              <Icon icon="move" color={Colors.GREEN4} />
-            </>
-          }
-        />
-        {/* CONTROL_MODES[CONTROL_MODES.MANUAL] */}
-
-        <Dropdown.Option
-          value={3}
-          text="Track"
-          labelElement={
-            <>
-              <Icon icon="flow-end" color={Colors.RED4} />
-            </>
-          }
-        />
-        {/* CONTROL_MODES[CONTROL_MODES.TRACK] */}
-
-        <Dropdown.Option
-          value={4}
-          text="Demo"
-          labelElement={
-            <>
-              <Icon icon="projects" color={Colors.GOLD4} />
-            </>
-          }
-        />
-        {/* CONTROL_MODES[CONTROL_MODES.DEMO] */}
-
-        <Dropdown.Option
-          value={2}
-          text="Run Program"
-          labelElement={
-            <>
-              <Icon icon="document-open" color={Colors.BLUE4} />
-            </>
-          }
-        />
-        {/* CONTROL_MODES[CONTROL_MODES.EVENT] */}
-      </Dropdown>
     </React.Fragment>
   )
 }
