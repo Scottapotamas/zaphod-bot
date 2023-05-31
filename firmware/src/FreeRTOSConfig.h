943ddf0cabec2ae565889e72c165bcfef5baa56a
Scott Rapson <scottapotamas@gmail.com>
2023-05-31 13:22:38 +0930

    Increase FreeRTOS timer task queue size

--- firmware/src/FreeRTOSConfig.h
+++ firmware/src/FreeRTOSConfig.h
@@ -69,7 +69,7 @@ extern uint32_t SystemCoreClock;
 /* Software timer definitions. */
 #define configUSE_TIMERS             1
 #define configTIMER_TASK_PRIORITY    ( 2 )
-#define configTIMER_QUEUE_LENGTH     10
+#define configTIMER_QUEUE_LENGTH     15
 #define configTIMER_TASK_STACK_DEPTH ( configMINIMAL_STACK_SIZE * 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
