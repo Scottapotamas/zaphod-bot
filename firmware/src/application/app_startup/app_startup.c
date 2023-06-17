409ef8cd7ee4a7237492c898a02e1c496c70659a
Scott Rapson <scottapotamas@gmail.com>
2023-06-18 00:33:48 +0930

    Broker setup and removed old observer init handling

--- firmware/src/application/app_startup/app_startup.c
+++ firmware/src/application/app_startup/app_startup.c
@@ -25,7 +25,7 @@
 #include "led_interpolator.h"
 #include "led.h"
 
-#include "subject.h"
+#include "broker.h"
 
 /* -------------------------------------------------------------------------- */
 
@@ -34,6 +34,8 @@ const uint8_t priority_low = priority_lowest + 1;
 const uint8_t priority_normal = priority_low + 1;
 const uint8_t priority_high = priority_normal + 1;
 const uint8_t priority_highest = priority_high + 1;
+
+Broker pubsub_broker = { 0 };
 
 /* -------------------------------------------------------------------------- */
 
@@ -49,6 +51,8 @@ void app_startup_init( void )
     user_interface_set_reset_cause( hal_reset_cause_description( hal_reset_cause() ) );
     user_interface_set_assert_cause( hal_reset_assert_description() );
 
+    broker_init( &pubsub_broker );
+
     sensors_init();
     fan_init();
     buzzer_init();
@@ -59,60 +63,18 @@ void app_startup_init( void )
     request_handler_init( REQUEST_HANDLER_FADES );
 
     path_interpolator_init();
-    Subject *pathing_events = path_interpolator_get_subject();
-
     point_follower_init();
-
     led_interpolator_init();
 
     user_interface_init();
-    Subject *ui_request_subject = user_interface_get_request_subject();
-
     effector_init();
-    Subject *effector_data = effector_get_subject();
-
     overwatch_init();
-    Subject *overwatch_commands = overwatch_get_subject();
 
-    // Attach observers to subjects as needed
-
-    // Overwatch subscriptions
-    subject_add_observer( ui_request_subject, overwatch_get_observer() );
-    subject_add_observer( pathing_events, overwatch_get_observer() );
-    subject_add_observer( effector_data, overwatch_get_observer() );
-
-    // Shutter release subscribes to overwatch and UI requests
-    subject_add_observer( ui_request_subject, shutter_get_observer() );
-    subject_add_observer( overwatch_commands, shutter_get_observer() );
-
-    // LED task subscriptions
-    subject_add_observer( effector_data, led_get_observer() );
-
-    // Telemetry task wants to know pretty much everything
-    sensors_add_observer( user_interface_get_observer() );
-    subject_add_observer( overwatch_commands, user_interface_get_observer() );
-    subject_add_observer( effector_data, user_interface_get_observer() );
-    subject_add_observer( pathing_events, user_interface_get_observer() );
-
-    subject_add_observer( request_handler_get_subject_for(REQUEST_HANDLER_MOVES), user_interface_get_observer() );
-    subject_add_observer( request_handler_get_subject_for(REQUEST_HANDLER_FADES), user_interface_get_observer() );
-
-    // Init all servos, setup inbound sensor data, commands, and output state updates
+    // Init all servos
     for( ClearpathServoInstance_t instance = _CLEARPATH_1; instance < _CLEARPATH_4; instance++ )    // _NUMBER_CLEARPATH_SERVOS
     {
         servo_init( instance );
-
-        sensors_add_observer( servo_get_observer(instance) );
-        subject_add_observer( overwatch_commands, servo_get_observer(instance) );
-        subject_add_observer( effector_data, servo_get_observer(instance) );
-
-        // Add overwatch and telemetry tasks to servo output subjects
-        subject_add_observer( servo_get_subject( instance ), overwatch_get_observer() );
-        subject_add_observer( servo_get_subject( instance ), user_interface_get_observer() );
     }
-
-    // Misc
-    sensors_add_observer( fan_get_observer() );
 
 }
 
@@ -120,6 +82,8 @@ void app_startup_init( void )
 
 void app_startup_tasks( void )
 {
+    xTaskCreate( broker_task, "pubsub", configMINIMAL_STACK_SIZE, NULL, priority_normal, NULL );
+
     xTaskCreate( sensors_task, "sensors", configMINIMAL_STACK_SIZE+200, NULL, priority_low, NULL );
     xTaskCreate( buzzer_task, "buzzer", configMINIMAL_STACK_SIZE, NULL, priority_lowest, NULL );
     xTaskCreate( fan_task, "fan", configMINIMAL_STACK_SIZE, NULL, priority_lowest, NULL );
