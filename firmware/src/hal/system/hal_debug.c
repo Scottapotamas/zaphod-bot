975b0683254990aa825b1de433fc8597e17187c4
Scott Rapson <scottapotamas@gmail.com>
2023-06-24 15:31:26 +0930

    Add rough ITM based debug output

---
+++ firmware/src/hal/system/hal_debug.c
@@ -0,0 +1,38 @@
+/* -------------------------------------------------------------------------- */
+
+#include "hal_debug.h"
+
+#include "stm32f4xx_ll_cortex.h"
+
+// String print requirements
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/* -------------------------------------------------------------------------- */
+char itm_buffer[100] = { 0 };
+
+PUBLIC void hal_debug_printf( const char *fmt, ... )
+{
+    va_list args;
+    char *ptr = &itm_buffer[0];
+
+    va_start(args, fmt);
+    vsnprintf( ptr, sizeof( itm_buffer ), fmt, args );
+    va_end(args);
+
+    while (*ptr != '\0')
+    {
+        ITM_SendChar(*ptr++);
+    }
+//    memset( &itm_buffer, 0, sizeof(itm_buffer) );
+}
+
+PUBLIC void hal_debug_print( const char *fmt )
+{
+    while (*fmt != '\0')
+    {
+        ITM_SendChar(*fmt++);
+    }
+}
+/* -------------------------------------------------------------------------- */
---
+++ firmware/src/hal/system/hal_debug.h
@@ -0,0 +1,24 @@
+#ifndef HAL_DEBUG_H
+#define HAL_DEBUG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* -------------------------------------------------------------------------- */
+
+#include "global.h"
+
+/* -------------------------------------------------------------------------- */
+
+PUBLIC void hal_debug_printf( const char *fmt, ... );
+
+PUBLIC void hal_debug_print( const char *fmt );
+
+/* -------------------------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    // HAL_DEBUG_H
